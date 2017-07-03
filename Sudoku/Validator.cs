dd3137448b1c24e4b32d2d1c33a2bdeed6449bfd
vachovia <vlad_petrosian@hotmail.com>
2017-07-03 17:48:54 -0400

    Validator - cleanup

--- Sudoku/Validator.cs
+++ Sudoku/Validator.cs
@@ -6,33 +6,33 @@ using System.Threading.Tasks;
 
 namespace Sudoku
 {
-    public static class Validator
-    {
-        public static bool isValidSudoku(List<List<int>> board)
-        {
-            for (int i = 0; i < 9; i++)
-            {
-                HashSet<int> rows = new HashSet<int>();
-                HashSet<int> columns = new HashSet<int>();
-                HashSet<int> cube = new HashSet<int>();
+    public static class Validator {
+        public static bool isValidSudoku(List<List<int>> board) {
+            try {
+                for (int i = 0; i < 9; i++) {
+                    HashSet<int> rows = new HashSet<int>();
+                    HashSet<int> columns = new HashSet<int>();
+                    HashSet<int> cube = new HashSet<int>();
 
-                for (int j = 0; j < 9; j++)
-                {
-                    if (!rows.Add(board[i][j]) || !columns.Add(board[j][i])) 
-                    {
-                        return false;
-                    }
+                    for (int j = 0; j < 9; j++) {
+                        if (!rows.Add(board[i][j]) || !columns.Add(board[j][i])) {
+                            return false;
+                        }
 
-                    int RowIndex = 3 * (i / 3); int ColIndex = 3 * (i % 3);
+                        int RowIndex = 3 * (i / 3); int ColIndex = 3 * (i % 3);
 
-                    if (!cube.Add(board[RowIndex + j / 3][ColIndex + j % 3]))
-                    {
-                        return false;
+                        if (!cube.Add(board[RowIndex + j / 3][ColIndex + j % 3])) {
+                            return false;
+                        }
                     }
                 }
+
+                return true;
             }
 
-            return true;
+            catch {
+                return false;
+            }
         }
     }
 }
